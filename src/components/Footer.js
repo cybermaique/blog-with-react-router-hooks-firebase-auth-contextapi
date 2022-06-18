2c5b5f98c0699836b80ceabbd81ac271ae1efe08
cybermaique <maiqued.18@gmail.com>
2022-06-18 16:20:51 -0300

    footer and navbar

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+   <footer>
+    <h3>Escrever publicação</h3>
+    <p>My Blog - cybermaique - &copy; 2022</p>
+   </footer>
+  )
+}
+
+export default Footer
